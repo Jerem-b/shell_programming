9d318a911b7967909d0eb7041a7248baf7be2d54
Jerem-b <58622587+Jerem-b@users.noreply.github.com>
2020-06-02 15:31:27 +0200

    Add files via upload

---
+++ PSU_minishell1_2019/my/my_strlen.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** minishell
+** File description:
+** my_strlen.c
+*/
+
+#include "../include/minishell.h"
+
+int my_strlen(char *str)
+{
+    int i = 0;
+
+    for (; str[i] != '\0'; i++){
+    }
+
+    return i;
+}
